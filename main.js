056fea71fb5fc38b297ef637dc1b526e8149e1ee
Mugdha <mugdha6123@gmail.com>
2024-10-19 13:17:40 +0530

    Assignment Completed

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-// you can import the module in your main.js file
-// eg: import Vue from 'vue' or import { createApp } from 'vue'
+const btn = document.getElementById('btn')
+btn.addEventListener('click', async () => {
+    try{
+        const response = await fetch('https://dummyjson.com/users')
+        const data = await response.json()
+        const result = data.users.filter(user => user.age > 30)
+        console.log(result)
+    }catch(error){
+        console.log("Error fetching users:",error)
+    }
+})
